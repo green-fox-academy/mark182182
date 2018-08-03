b042573a8c6ee4e0a9849d906357c16f889701e3
mark182182 <versalife11@hotmail.com>
2018-08-03 12:41:04 +0200

    fix(loops): Replacing the loops

--- week-01/day-04/draw-diamond/draw-diamond.ts
+++ week-01/day-04/draw-diamond/draw-diamond.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-//let lineCount: number = 7;
+let lineCount: number = 7;
 
 // Write a program that draws a
 // diamond like this:
@@ -15,22 +15,21 @@
 //
 // The diamond should have as many lines as lineCount is
 
-let drawColums = 7;
-//let drawSpaces = (lineCount / 2);
-for (let drawRows = 1; drawRows <= lineCount; drawRows++) {
-    for (let k = drawSpaces; k >= 1; k--) {
-        console.log("!");
-    }
-    for (let j = 1; j <= drawColums; j++) {
-        console.log("*");
-    }
-    console.log('!');
-    if (drawRows < (lineCount / 2) + 1) {
-        drawColums += 2;
-        drawSpaces -= 1;
-    } else {
-        drawColums -= 2;
-        drawSpaces += 1;
+for (let i = 1; i <= (lineCount -3); i++) {
+    let row = '';
+
+    for (let j = 1; j <= ((lineCount -3) - i); j++) {
+        row += ' ';
 
     }
+
+    for (let k = 1; k <= i; k++) {
+        if (k === 1) {
+             row += '*' 
+            }
+        else { 
+            row += '**'; 
+        }
+    }
+    console.log(row);
 }
