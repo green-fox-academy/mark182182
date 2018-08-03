e55f0e08b2210cba872ea840e0fce9a83eb8c696
mark182182 <versalife11@hotmail.com>
2018-08-03 16:55:05 +0200

    feat(squareloops): made 2 loops to move the square because reasons

--- week-01/day-04/draw-square/draw-square.ts
+++ week-01/day-04/draw-square/draw-square.ts
@@ -1,8 +1,6 @@
 export { }
 'use strict';
 
-let lineCount: number = 6;
-
 // Write a program that draws a square like this:
 //
 // %%%%%%
@@ -14,24 +12,36 @@ let lineCount: number = 6;
 //
 // The square should have as many lines as lineCount is
 
-for (let a: number = 0; a <= lineCount; a++) {
-    let row: string = '';
-    for (let b: number = 0; b <= lineCount; b++) {
-        if (b === 0) {
-            row += '%';
+for (let a: number = 0; a <= 10; a++) {
+    drawSquare(a);
+}
+for (let a: number = 9; a <= 9 && a >= 0; a--) {
+    drawSquare(a);
+}
+
+function drawSquare(drawSize: number) {
+    let lineCount: number = 0;
+    lineCount = drawSize;
+    for (let a: number = 0; a <= lineCount; a++) {
+        let row: string = '';
+        for (let b: number = 0; b <= lineCount; b++) {
+            if (b === 0) {
+                row += '%';
+            }
+            else if (b === lineCount) {
+                row += '%';
+            }
+            else if (a === 0) {
+                row += '%';
+            }
+            else if (a === lineCount) {
+                row += '%';
+            }
+            else {
+                row += ' ';
+            }
         }
-        else if (b === 6) {
-            row += '%';
-        }
-        else if (a === 0) {
-            row += '%';
-        }
-        else if (a === 6) {
-            row += '%';
-        }
-        else {
-            row += ' ';
-        }
+        console.log(row);
     }
-    console.log(row);
+    return drawSize;
 }
