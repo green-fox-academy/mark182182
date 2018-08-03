e738292d2f5c7c37f8d383df623746b6e764dd0b
mark182182 <versalife11@hotmail.com>
2018-08-03 16:43:15 +0200

    feat(drawSize): function for creating the square object

---
+++ week-01/day-04/draw-chess-table/draw-chess-table.ts
@@ -0,0 +1,20 @@
+export {}
+
+'use strict';
+
+// Create a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+//
+
+let lineCount: number = 8,
+
+for (let a: number = )
+{}
