69b4132eccf34ee78a3fd2f4e8f180f6471199fb
mark182182 <versalife11@hotmail.com>
2018-08-03 14:04:18 +0200

    fix(forloops): Final version of the for loops

--- week-01/day-04/draw-diamond/draw-diamond.ts
+++ week-01/day-04/draw-diamond/draw-diamond.ts
@@ -15,20 +15,35 @@ let lineCount: number = 7;
 //
 // The diamond should have as many lines as lineCount is
 
-for (let i = 1; i <= (lineCount -3); i++) {
+for (let i = 1; i <= (lineCount - 3); i++) {
     let row = '';
 
-    for (let j = 1; j <= ((lineCount -3) - i); j++) {
+    for (let j = 1; j <= ((lineCount - 3) - i); j++) {
         row += ' ';
 
     }
 
     for (let k = 1; k <= i; k++) {
         if (k === 1) {
-             row += '*' 
-            }
-        else { 
-            row += '**'; 
+            row += '*'
+        }
+        else {
+            row += '**';
+        }
+    }
+    console.log(row);
+}
+for (let h = 5; h <= lineCount; h++) {
+    let row = '';
+    for (let j = 5; j <= h; j++) {
+        row += ' ';
+    }
+    for (let k = 7; k >= h; k--) {
+        if (k === 7) {
+            row += '*'
+        }
+        else {
+            row += '**';
         }
     }
     console.log(row);
--- week-01/day-04/draw-pyramid/draw-pyramid.ts
+++ week-01/day-04/draw-pyramid/draw-pyramid.ts
@@ -1,6 +1,8 @@
+export { }
+
 'use strict';
 
-//let lineCount: number = 4;
+let lineCount: number = 4;
 
 // Write a program that draws a
 // pyramid like this:
@@ -22,10 +24,10 @@ for (let i = 1; i <= lineCount; i++) {
 
     for (let k = 1; k <= i; k++) {
         if (k === 1) {
-             row += '*' 
-            }
-        else { 
-            row += '**'; 
+            row += '*'
+        }
+        else {
+            row += '**';
         }
     }
     console.log(row);
