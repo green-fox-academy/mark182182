6233464d36b8c42333feeeda1ec735e52100b95e
mark182182 <versalife11@hotmail.com>
2018-08-03 10:56:20 +0200

    Trying to solve the pyramid task

---
+++ week-01/day-04/draw-diamond/draw-diamond.ts
@@ -0,0 +1,36 @@
+'use strict';
+
+let lineCount: number = 7;
+
+// Write a program that draws a
+// diamond like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+//
+// The diamond should have as many lines as lineCount is
+
+let drawColums = 7;
+let drawSpaces = (lineCount / 2);
+for (let drawRows = 1; drawRows <= lineCount; drawRows++) {
+    for (let k = drawSpaces; k >= 1; k--) {
+        console.log("!");
+    }
+    for (let j = 1; j <= drawColums; j++) {
+        console.log("*");
+    }
+    console.log();
+    if (drawRows < lineCount / 2 + 1) {
+        drawColums += 2;
+        drawSpaces -= 1;
+    } else {
+        drawColums -= 2;
+        drawSpaces += 1;
+
+    }
+}
--- week-01/day-04/draw-pyramid/draw-pyramid.ts
+++ week-01/day-04/draw-pyramid/draw-pyramid.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-let lineCount: number = 4;
+//let lineCount: number = 4;
 
 // Write a program that draws a
 // pyramid like this:
@@ -11,10 +11,21 @@ let lineCount: number = 4;
 // *******
 //
 // The pyramid should have as many lines as lineCount is
+let drawRows: number = 0;
 let drawShapes: string = '';
-let drawSpaces: string = '    ';
+// let drawSpaces: string = '';
+let substractSpaces: number = 0;
 
-for (drawSpaces; drawSpaces.length <= lineCount; ) {
+for (drawRows; drawRows <= lineCount; drawRows++) {
+    for (drawSpaces; drawSpaces.length <= (lineCount - drawRows); drawSpaces += '!') {
+        if (drawSpaces.length == (lineCount - drawRows)) {
+            for (drawSpaces; substractSpaces <= lineCount; substractSpaces++) {
+                console.log(drawSpaces.substr(substractSpaces, drawSpaces.length));
+                for (drawShapes; drawShapes.length <= 4; drawShapes += '*') {
+                    console.log(drawShapes);
+                }
+            }
+        }
 
-    console.log(drawSpaces + (drawShapes += '*'));
+    }
 }
