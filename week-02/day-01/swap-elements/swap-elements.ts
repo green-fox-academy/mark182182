5f731085c01ae128ef84140c296a6d52860cdad5
mark182182 <versalife11@hotmail.com>
2018-08-06 11:32:17 +0200

    feat(swap): Swaps the first and the third element

---
+++ week-02/day-01/swap-elements/swap-elements.ts
@@ -0,0 +1,9 @@
+'use strict';
+export { };
+
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+let abc = ["Arthur","Boe","Chloe"];
+abc.reverse();
+console.log(abc);
