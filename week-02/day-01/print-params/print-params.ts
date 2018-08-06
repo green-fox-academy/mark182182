92d851ac6cd8bee339c170b4a7fdf68d5b6e09eb
mark182182 <versalife11@hotmail.com>
2018-08-06 11:00:54 +0200

    feat(print-params): Added function to print any type of input

---
+++ week-02/day-01/print-params/print-params.ts
@@ -0,0 +1,12 @@
+'use strict';
+export { };
+
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+function printParams(firstInput: any, secondInput: any){
+    return console.log(firstInput,secondInput);
+}
+
+printParams('hello', 'myfrend');
