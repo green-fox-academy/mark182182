2b485a82305c949241c16d135d5c3febe148f0cf
mark182182 <versalife11@hotmail.com>
2018-08-07 14:12:37 +0200

    feat(is-in-list): checks if the numbers are in the array

---
+++ week-02/day-02/is-in-list/is-in-list.ts
@@ -0,0 +1,22 @@
+'use strict';
+export { };
+
+// Check if array contains all of the following elements: 4,8,12,16
+// Create a function that accepts 'listOfNumbers' as an input
+// it should return "true" if it contains all, otherwise "false"
+
+let listOfNumbers: number[] = [2, 4, 6, 8, 10, 12, 14, 16];
+
+function checkNums(listOfNumbers: number[]) {
+    let isInList: boolean = undefined;
+    isInList = listOfNumbers.some(function (element): boolean {
+        if (element === 4 && 8 && 12 && 16) {
+            return true;
+        }
+    })
+    return isInList;
+}
+
+console.log(checkNums(listOfNumbers));
+
+export = checkNums;
