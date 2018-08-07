a518d2e3fbf6f7d02b606d43aacceaa2c579d6f6
mark182182 <versalife11@hotmail.com>
2018-08-07 08:27:56 +0200

    feat(url-fixer): fixedURL contains fixed string

---
+++ week-02/day-02/url-fixer/url-fixer.ts
@@ -0,0 +1,10 @@
+'use strict';
+export { }
+
+// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+// Also, the URL is missing a crutial component, find out what it is and insert it too!
+let url: string = 'https//www.reddit.com/r/nevertellmethebots';
+
+let fixedURL = url.replace('bots', 'odds');
+
+console.log(fixedURL);
