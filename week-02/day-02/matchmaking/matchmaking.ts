6b5373d956f13dda5d1d17a0d007adfd57dfb5c6
mark182182 <versalife11@hotmail.com>
2018-08-07 12:31:49 +0200

    feat(matchmaking): pairs the girls with the boys

---
+++ week-02/day-02/matchmaking/matchmaking.ts
@@ -0,0 +1,30 @@
+'use strict';
+export { };
+
+// Write a function that joins two array by matching one girl with one boy in a new array
+// Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
+
+let girls: string[] = ['Eve', 'Ashley', 'Claire', 'Kat', 'Jane'];
+let boys: string[] = ['Joe', 'Fred', 'Tom', 'Todd', 'Neef', 'Jeff'];
+
+function makingMatches(girls: string[], boys: string[]): string[] {
+    let matchMaking: string[] = [];
+    for (let element = 0; element < boys.length; element++) {
+        if (girls[element] != undefined) {
+            matchMaking.push(girls[element]);
+            matchMaking.push(boys[element]);
+        }
+        else {
+            matchMaking.push(boys[element]);
+        }
+
+        console.log(matchMaking);
+        console.log(element);
+    }
+    return matchMaking;
+
+}
+
+console.log(makingMatches(girls, boys));
+
+export = makingMatches;
