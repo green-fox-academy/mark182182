b01112be5dc3e9563e775fb682c3360d3cb6c179
mark182182 <versalife11@hotmail.com>
2018-08-08 07:18:54 +0200

    feat(centered-square): draw a square to the center with bonus red stroke

---
+++ week-02/day-03/centered-square/centered-square-ts.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Draw a green 10x10 square to the center of the canvas.
+
+
+ctx.fillStyle = 'green';
+ctx.fillRect(295, 195, 10, 10);
+
+//Bonus
+
+ctx.strokeStyle = 'red';
+ctx.strokeRect(250, 150, 100, 100);
