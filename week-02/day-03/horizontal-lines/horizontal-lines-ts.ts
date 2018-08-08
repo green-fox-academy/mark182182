598d51e89377f96d1fa63e8143995556c6d1223b
mark182182 <versalife11@hotmail.com>
2018-08-08 07:12:08 +0200

    feat(horizontal-line): draw 3 horizontal lines

---
+++ week-02/day-03/horizontal-lines/horizontal-lines-ts.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// create a line drawing function that takes 2 parameters:
+// the x and y coordinates of the line's starting point
+// and draws a 50 long horizontal line from that point.
+// draw 3 lines with that function. Use loop for that.
+
+function lineDraw(xCord: number, yCord: number) {
+    ctx.strokeStyle = 'green';
+    ctx.beginPath();
+    ctx.moveTo(xCord, yCord);
+    ctx.lineTo(50, yCord);
+    ctx.stroke();
+
+}
+
+for (let currentCord = 1; currentCord <= 3; currentCord++) {
+    lineDraw(100, currentCord * 100);
+}
