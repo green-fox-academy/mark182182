c58d787219b02a41273d10545dc0eda229583015
mark182182 <versalife11@hotmail.com>
2018-08-09 16:35:43 +0200

    feat(drawShapws): draw number of shapes divided by canvas

---
+++ week-02/day-04/line-play-quarter/line-play-quarter.ts
@@ -0,0 +1,89 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+const renderTime = document.getElementById('renderTime');
+const numberOfLines = document.getElementById('start');
+const showLineRange = document.getElementById('showLineRange');
+const setButton = document.getElementById('setButton');
+
+let useButton = setButton.addEventListener("click", function (e) {
+    location.reload();
+});
+
+let divideCanvasBy: number = 100;
+
+divideCanvasBy = numberOfLines.value;
+showLineRange.value = divideCanvasBy;
+
+window.addEventListener("load", loadTime, false);
+
+function loadTime() {
+    let now = new Date().getTime();
+    let page_load_time = (now - performance.timing.navigationStart);
+    if (window.console) {
+        renderTime.value = (page_load_time / 1000);
+    }
+}
+
+if (divideCanvasBy > canvas.height || divideCanvasBy > canvas.width) {
+    canvas.height = divideCanvasBy;
+    canvas.width = divideCanvasBy;
+}
+
+function putInside(): void {
+    for (let drawRows: number = 0; drawRows < canvas.width; drawRows += canvas.width / divideCanvasBy) {
+        for (let drawShapes: number = 0; drawShapes < canvas.width; drawShapes++) {
+            makeShape(canvas.width / divideCanvasBy * drawShapes, drawRows);
+        }
+    }
+}
+
+function divideCanvas(): void {
+    ctx.beginPath();
+    for (let drawLines: number = 1; drawLines < canvas.width; drawLines++) {
+        if (drawLines % (canvas.width / divideCanvasBy) == 0) {
+            ctx.moveTo(drawLines, 0);
+            ctx.lineTo(drawLines, canvas.width);
+        }
+    }
+    for (let drawLines: number = 1; drawLines < canvas.width; drawLines++) {
+        if (drawLines % (canvas.width / divideCanvasBy) == 0) {
+            ctx.moveTo(0, drawLines);
+            ctx.lineTo(canvas.width, drawLines);
+        }
+        ctx.stroke();
+    }
+}
+
+function makeShape(referenceXPoint: number, referenceYPoint: number): void {
+    drawBottomLine(referenceXPoint, referenceYPoint);
+    drawTopLine(referenceXPoint, referenceYPoint);
+}
+
+function drawBottomLine(referenceXPoint: number, referenceYPoint: number): void {
+    ctx.strokeStyle = 'green';
+    ctx.beginPath();
+    for (let drawBottom: number = 0; drawBottom <= canvas.width / divideCanvasBy; drawBottom++) {
+        if (drawBottom % 2 == 0) {
+            ctx.moveTo(referenceXPoint, drawBottom + referenceYPoint);
+            ctx.lineTo(drawBottom + referenceXPoint, canvas.width / divideCanvasBy + referenceYPoint);
+        }
+        ctx.stroke();
+    }
+}
+
+function drawTopLine(referenceXPoint: number, referenceYPoint: number): void {
+    ctx.strokeStyle = 'purple';
+    ctx.beginPath();
+    for (let drawTop: number = 0; drawTop <= canvas.width / divideCanvasBy; drawTop++) {
+        if (drawTop % 2 == 0) {
+            ctx.moveTo(drawTop + referenceXPoint, referenceYPoint);
+            ctx.lineTo(canvas.width / divideCanvasBy + referenceXPoint, drawTop + referenceYPoint);
+        }
+        ctx.stroke();
+    }
+}
+
+divideCanvas();
+putInside();
