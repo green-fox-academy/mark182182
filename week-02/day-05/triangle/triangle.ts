cafbfb297397214d522adf1958096737415a17c9
mark182182 <versalife11@hotmail.com>
2018-08-13 07:34:30 +0200

    fix(triangle): it now positions the vertical lines correctly

--- week-02/day-05/triangle/triangle.ts
+++ week-02/day-05/triangle/triangle.ts
@@ -8,7 +8,7 @@ let triangleSide: number = 0;
 function drawTriangle() {
   ctx.beginPath();
   for (triangleSide; triangleSide <= canvas.height; triangleSide++) {
-    if (triangleSide % 20 == 0) {
+    if (triangleSide % 120 == 0) {
       ctx.moveTo(canvas.height / 2 + triangleSide / 2, triangleSide);
       ctx.lineTo(triangleSide, canvas.height + triangleSide);
       ctx.moveTo(canvas.height / 2 - triangleSide / 2, triangleSide);
@@ -16,7 +16,7 @@ function drawTriangle() {
     }
   }
   for (let drawSeparator = 0; drawSeparator <= canvas.height; drawSeparator++) {
-    if (drawSeparator % (Math.round(Math.sqrt(3) / 2)) == 0) {
+    if (drawSeparator % 120 == 0) {
       ctx.moveTo(drawSeparator / 2, canvas.height - drawSeparator);
       ctx.lineTo(canvas.height - drawSeparator / 2, canvas.height - drawSeparator);
     }
