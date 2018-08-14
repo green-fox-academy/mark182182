42a9553cbae3cc0a91af75384371cc43e05d42d7
mark182182 <versalife11@hotmail.com>
2018-08-14 13:38:47 +0200

    feat(logs): create txt file

--- week-03/day-02/logs/logs.ts
+++ week-03/day-02/logs/logs.ts
@@ -6,3 +6,32 @@ export { };
 // Write a function that returns an array with the unique IP adresses.
 // Write a function that returns the GET / POST request ratio.
 
+declare function require(path: string): any;
+
+const fs = require('fs');
+const charEncode: string = 'utf-8';
+
+function readAFile(fileName: string, charEncode: string) {
+  return fs.readFileSync(fileName, charEncode);
+}
+
+function countLines(fileName: string, charEncode: string): number {
+  try {
+    if (fs.existsSync(fileName)) {
+      const fileContent: string[] = readAFile(fileName, charEncode).split('\r\n');
+      let countLines: number = 0;
+      fileContent.forEach(elem => {
+        if (elem) {
+          countLines++;
+        }
+      })
+      console.log(countLines);
+    }
+  }
+  catch (e) {
+    return 0;
+  }
+
+}
+
+countLines('my-file.txt', charEncode);
