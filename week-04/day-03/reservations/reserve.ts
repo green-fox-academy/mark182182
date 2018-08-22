14277278f5b81a5fc0bffc93cd72b534dec77ab5
mark182182 <versalife11@hotmail.com>
2018-08-22 18:27:18 +0200

    fix(day-01,day-02): file relocation

--- week-04/day-03/reservations/reserve.ts
+++ week-04/day-03/reservations/reserve.ts
@@ -7,7 +7,4 @@ let newReservation: Reservation = new Reservation();
 for (let fillListWithItems: number = 0; fillListWithItems < 10; fillListWithItems++) {
   newReservation.addReservation(generateCode());
 }
-// for (let bookingIndex: number = 0; bookingIndex < newReservation.getReservationList().length; bookingIndex++) {
-//   console.log(`Booking# ${newReservation.getCodeBooking()} for ${newReservation.getDowBooking()}`);
-// }
-console.log(newReservation.getDowBooking());
+console.log(`Booking# ${newReservation.getCodeBooking()} for ${newReservation.getDowBooking()}`);
