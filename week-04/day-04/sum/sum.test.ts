fd8437e95283a2a13ab8c4d6a41b472011644039
mark182182 <versalife11@hotmail.com>
2018-08-23 14:44:25 +0200

    feat(sum.test): list with different elements

--- week-04/day-04/sum/sum.test.ts
+++ week-04/day-04/sum/sum.test.ts
@@ -3,3 +3,38 @@
 import { Sum } from './sum';
 
 const test = require('tape');
+
+test('This checks if numbers are added correctly', t => {
+  const sumAll: Sum = new Sum();
+  const testIntegers: number[] = [1, 2, 3];
+  t.deepEqual(sumAll.sumOfTheElements(testIntegers), [6], 'Should be equal');
+  t.end();
+});
+
+test('This checks if numbers are added correctly', t => {
+  const sumAll: Sum = new Sum();
+  const testIntegers: number[] = [];
+  t.deepEqual(sumAll.sumOfTheElements(testIntegers), [], 'Should be equal');
+  t.end();
+});
+
+test('This checks if numbers are added correctly', t => {
+  const sumAll: Sum = new Sum();
+  const testIntegers: number[] = [1];
+  t.deepEqual(sumAll.sumOfTheElements(testIntegers), [1], 'Should be equal');
+  t.end();
+});
+
+test('This checks if numbers are added correctly', t => {
+  const sumAll: Sum = new Sum();
+  const testIntegers: number[] = [null];
+  t.deepEqual(sumAll.sumOfTheElements(testIntegers), [null], 'Should be equal');
+  t.end();
+});
+
+test('This checks if numbers are added correctly', t => {
+  const sumAll: Sum = new Sum();
+  const testIntegers: number[] = [1, 2, 3];
+  t.notEqual(sumAll.sumOfTheElements(testIntegers), ['a string'], 'Should not be equal');
+  t.end();
+});
--- week-04/day-04/sum/sum.ts
+++ week-04/day-04/sum/sum.ts
@@ -1,9 +1,20 @@
 'use strict';
 
 export class Sum {
-  sumOfTheElements(listOfIntegers: number[]) {
+  sumOfTheElements(listOfIntegers: number[]): number[] {
+    let addValuesTogether: number = 0;
+    let sumOfListElements: number[] = [];
     listOfIntegers.forEach((value, index, array) => {
-      return value + value;
+      if (index < 1) {
+        return addValuesTogether = value;
+      }
+      else {
+        return addValuesTogether = value + value;
+      }
     });
+    if (addValuesTogether !== 0) {
+      sumOfListElements.push(addValuesTogether);
+    }
+    return sumOfListElements;
   };
-};
+}
