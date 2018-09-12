9ddbf089140f3c98656ea03b368b28bb1b1cd0b9
mark182182 <mark182182>
2018-09-12 12:50:52 +0200

    feat(queryselector): conceitedKing and noBusiness store classes

--- week-06/day-03/queryselector-1.js
+++ week-06/day-03/queryselector-1.js
@@ -4,3 +4,7 @@ let king = document.querySelector('#b325.asteroid');
 console.log(king);
 let businessLamp = document.querySelectorAll('.asteroid.big,  .asteroid.b329.big');
 console.log(businessLamp[0], businessLamp[1]);
+let conceitedKing = document.querySelectorAll('#b325.asteroid, .asteroid.b326')
+console.log(conceitedKing[0], conceitedKing[1]);
+let noBusiness = document.querySelectorAll('#b325.asteroid, .asteroid.b326, .asteroid.b329.big')
+console.log(noBusiness[0], noBusiness[1], noBusiness[2]);
