7ba56a3a275bf64e8b2a0d312ce54763eeac893b
mark182182 <mark182182>
2018-09-12 12:43:24 +0200

    feat(queryselector): store king and businessLamp in variables

---
+++ week-06/day-03/queryselector-1.js
@@ -0,0 +1,6 @@
+'use strict';
+
+let king = document.querySelector('#b325.asteroid');
+console.log(king);
+let businessLamp = document.querySelectorAll('.asteroid.big,  .asteroid.b329.big');
+console.log(businessLamp[0], businessLamp[1]);
