46857e7607e779a6658506bcef5fc0a576f6d9cd
mark182182 <mark182182>
2018-09-12 13:42:46 +0200

    feat(selectPar): replaces all text with last line

--- week-06/day-03/content1/content1.js
+++ week-06/day-03/content1/content1.js
@@ -1,1 +1,7 @@
 'use strict';
+
+let selectPar = document.querySelectorAll('p');
+
+for (let index = 0; index < selectPar.length - 1; index++) {
+  selectPar[index].innerHTML = selectPar[3].innerHTML;
+}
