e2aea0d0b76a5370b14018cd9fa310b49c036684
mark182182 <mark182182>
2018-09-17 18:54:36 +0200

    feat(translatedText): translate input text if language is hu

--- week-07/day-01/camel/app.js
+++ week-07/day-01/camel/app.js
@@ -8,7 +8,22 @@ const PORT = 8080;
 app.use(bodyParser.json());
 
 app.post('/translate', (req, res) => {
-  res.send('Working.');
+  let text = req.body.text;
+  let language = req.body.lang;
+
+  if (text !== undefined && language === 'hu') {
+    let splitText = text.split('');
+    let translatedText = splitText.map((value, index, array) => {
+      if (value.match('a|á|e|é|o|A|Á|E|É|O') !== null) {
+        return value = value + 'v' + value.toLowerCase();
+      }
+      else
+        return value;
+    });
+    res.json({
+      translated: translatedText.join(''),
+    });
+  }
 });
 
 app.listen(PORT, () => {
