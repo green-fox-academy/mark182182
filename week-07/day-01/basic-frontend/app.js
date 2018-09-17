5b5c3f07ed6dadd9ed9bb071fad1e8f11b5dc5be
mark182182 <mark182182>
2018-09-17 13:02:19 +0200

    feat(greeter): prints message if name and title is present

--- week-07/day-01/basic-frontend/app.js
+++ week-07/day-01/basic-frontend/app.js
@@ -18,12 +18,31 @@ app.get('/doubling', (req, res) => {
   if (input) {
     res.json({
       "received": input,
-      "result": input * 2
+      "result": input * 2,
     });
   }
   else {
     res.json({
       'error': 'Please provide an input!',
+    });
+  }
+});
+
+app.get('/greeter', (req, res) => {
+  let name = req.query.name;
+  let title = req.query.title;
+  if (name && title)
+    res.json({
+      "welcome_message": `Oh, hi there ${name}, my dear student!`,
+    });
+  else if (name === undefined && title) {
+    res.json({
+      "error": "Please provide a name!",
+    })
+  }
+  else if (title === undefined && name) {
+    res.json({
+      "error": "Please provide a title!",
     });
   }
 });
