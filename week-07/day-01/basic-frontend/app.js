cac0de96a273d57f8fd574893309f4919289ec2c
mark182182 <mark182182>
2018-09-17 12:51:52 +0200

    feat(doubling): received number is doubled

--- week-07/day-01/basic-frontend/app.js
+++ week-07/day-01/basic-frontend/app.js
@@ -3,12 +3,29 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 const PORT = 8080;
 
+app.use(bodyParser.json());
 app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/doubling', (req, res) => {
+  let input = req.query.input;
+  if (input) {
+    res.json({
+      "received": input,
+      "result": input * 2
+    });
+  }
+  else {
+    res.json({
+      'error': 'Please provide an input!',
+    });
+  }
 });
 
 app.listen(PORT, () => {
