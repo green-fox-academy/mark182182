8460168a7b1d460b9815536c9d4fd2deca22f447
mark182182 <mark182182>
2018-09-20 15:37:25 +0200

    feat(app.post(/posts)): create new record in db

--- week-07/day-04/reddit/assets/js/reddit.js
+++ week-07/day-04/reddit/assets/js/reddit.js
@@ -6,10 +6,22 @@ window.onload = () => {
   http.onload = () => {
     if (http.status === 200) {
       const context = JSON.parse(http.response);
-      
+      getPosts(context);
     }
   }
 
   http.send();
 
+}
+
+function getPosts(context) {
+  const getPostsContainer = document.querySelector('.posts-container');
+  for (let index = 0; index < context.length; index++) {
+    let newElement = document.createElement('a');
+    newElement.classList.add(`post${index}`);
+    getPostsContainer.appendChild(newElement);
+    newElement.innerHTML = context[index].title + ' ' + context[index].vote;
+    newElement.classList.add('bg-white', 'text-dark', 'shadow', 'd-block', 'border');
+    newElement.setAttribute('href', context[index].url);
+  }
 }
--- week-07/day-04/reddit/server.js
+++ week-07/day-04/reddit/server.js
@@ -24,19 +24,62 @@ conn.connect((err) => {
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.status(200).sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/posts', function (req, res) {
-  conn.query(`SELECT * from posts WHERE id BETWEEN 25 AND 74`, function (err, rows) {
+  req.headers.accept = 'application/json';
+  conn.query(`SELECT * from posts`, function (err, rows) {
     if (err) {
       console.log(err.toString());
       res.status(500).send('Database error');
       return;
     }
-    res.json(rows);
+    res.status(200).json(rows);
   });
 });
+
+app.post('/posts', function (req, res) {
+  req.headers.accept = 'application/json';
+  const title = req.body.title;
+  const url = req.body.url;
+  const username = req.body.username;
+  conn.query(`INSERT INTO posts (title, url, owner) VALUES('${title}','${url}', '${username}')`, function (err) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      return;
+    }
+    else {
+      console.log(`Inserted ${req.body.title}`);
+      conn.query(`SELECT * from posts WHERE title = '${title}' ORDER BY id DESC LIMIT 1`, function (err, rows) {
+        if (err) {
+          console.log(err.toString());
+          res.status(500).send('Database error');
+          return;
+        }
+        res.status(200).json(rows);
+      });
+    }
+  });
+});
+
+/* app.put('/posts/:id/upvote', function (req, res) {
+  const postId = req.param.id;
+  const vote = req.body.vote;
+  req.headers.accept = 'application/json';
+  conn.query(`UPDATE posts SET vote = ${vote + 1} WHERE id = ${postId}`, function (err) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      return;
+    }
+    else {
+      console.log(`Upvote on id ${postId}`);
+    }
+    res.json({ rows });
+  });
+}); */
 
 app.listen(PORT, () => {
   console.log(`Server is up on ${PORT}`);
