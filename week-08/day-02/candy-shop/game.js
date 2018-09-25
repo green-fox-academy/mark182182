aa8e964dad087040639e38a9967bf879aba6e5d2
mark182182 <mark182182>
2018-09-25 17:30:01 +0200

    feat(pepper shop) create, buy and rain peppers

--- week-08/day-02/candy-shop/game.js
+++ week-08/day-02/candy-shop/game.js
@@ -1,9 +1,39 @@
 'use strict';
 
 window.onload = () => {
-  let numberOfPeppers = 1;
   const getPeppers = document.querySelector('.peppers');
   const addPeppers = document.querySelector('.create-peppers');
+  const getHotpeppers = document.querySelector('.hotpeppers');
+  const addHotpeppers = document.querySelector('.buy-hotpeppers');
+  const getSpeed = document.querySelector('.speed');
+  const pepperMachine = document.querySelector('.pepper-machine');
+
+  let numberOfPeppers = 1000;
+  getPeppers.innerHTML = numberOfPeppers;
+  let numberOfHotpeppers = 3;
+  let pepperSpeed = 0;
+  getSpeed.innerHTML = ~~(numberOfHotpeppers / 10);
+
   addPeppers.addEventListener('click', () => { getPeppers.innerHTML = numberOfPeppers++ }, false);
-  console.log(numberOfPeppers);
+  addHotpeppers.addEventListener('click', () => {
+    if (getPeppers.innerHTML >= 100) {
+      numberOfHotpeppers++;
+      numberOfPeppers += -100;
+      getHotpeppers.innerHTML += '🌶';
+      getPeppers.innerHTML = numberOfPeppers;
+    }
+    if (numberOfHotpeppers >= 10 && numberOfHotpeppers % 10 === 0) {
+      pepperSpeed++;
+    }
+  }, false);
+
+  pepperMachine.addEventListener('click', () => {
+    pepperSpeed *= 10;
+  }, false);
+
+
+  setInterval(() => {
+    numberOfPeppers += pepperSpeed;
+    getPeppers.innerHTML = numberOfPeppers;
+  }, 1000);
 }
