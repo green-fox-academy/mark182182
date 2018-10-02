3e2f3c85bd0408d76fe4091890adee42d0f36a65
mark182182 <versalife11@hotmail.com>
2018-10-02 14:04:18 +0200

    feat(promise): return name if it is resolved

---
+++ week-09/day-02/assets/index.js
@@ -0,0 +1,22 @@
+'use strict';
+window.onload = () => {
+  const newText = document.createElement('p');
+  document.querySelector('body').appendChild(newText);
+  const host = `https://uinames.com/api/`;
+
+  const myAsyncAjax = (url) => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.onload = () => resolve(xhr.response);
+      xhr.onerror = () => reject(xhr.statusText);
+      xhr.send();
+    });
+  };
+  /* .then((resp) => resp.json().then(resp => { */
+  myAsyncAjax(host).then((response) => {
+    newText.innerHTML = JSON.parse(response).name;
+  }).catch((error) => {
+    newText.innerHTML = 'error';
+  });
+}
