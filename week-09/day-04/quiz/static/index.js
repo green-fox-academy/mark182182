f526fada4ddde5adcf65586715ef541565237467
mark182182 <versalife11@hotmail.com>
2018-10-05 06:46:25 +0200

    feat(quiz): wrong and right answers are colored

--- week-09/day-04/quiz/static/index.js
+++ week-09/day-04/quiz/static/index.js
@@ -2,7 +2,7 @@
 
 window.onload = () => {
 
-  let score = localStorage.getItem('score');
+  const score = localStorage.getItem('score');
 
   if (score === undefined) {
     localStorage.setItem('score', 0);
@@ -11,8 +11,24 @@ window.onload = () => {
     localStorage.setItem('score', parseInt(score) + 1);
   }
 
-  let h2 = document.querySelector('h2').innerHTML = 'Score ' + score;
+  const reloadCount = localStorage.getItem('reload');
 
+  if (reloadCount === undefined) {
+    localStorage.setItem('reload', 0);
+  }
+  else {
+    localStorage.setItem('reload', parseInt(reloadCount) + 1);
+  }
+
+  if (localStorage.getItem('reload') === 5) {
+    localStorage.setItem('reload', 0);
+    localStorage.setItem('score', 0);
+  }
+
+  console.log(reloadCount);
+
+
+  const h2 = document.querySelector('h2').innerHTML = 'SCORE: ' + score;
 
   const host = 'http://localhost:5000',
     http = new XMLHttpRequest;
@@ -22,11 +38,24 @@ window.onload = () => {
   http.onload = () => {
     const context = JSON.parse(http.response);
     for (let index = 0; index < context.answers.length; index++) {
-      let getAnswer = document.querySelector(`#ans${index + 1}`);
+      const getAnswer = document.querySelector(`#ans${index + 1}`);
+      getAnswer.classList.add(`state-${context.answers[index][3]}`);
       getAnswer.innerHTML = context.answers[index][2];
     }
+    const h3 = document.querySelector('h3').innerHTML = context.answers[0][5];
     console.log(context.answers[0]);
-  };
 
+    const getWrongAnswers = document.querySelectorAll('.state-0');
+    for (let index = 0; index < getWrongAnswers.length; index++) {
+      getWrongAnswers[index].addEventListener('click', () => {
+        getWrongAnswers[index].style.backgroundColor = 'tomato';
+      }, false);
+    }
+
+    const getRightAnswer = document.querySelector('.state-1');
+    getRightAnswer.addEventListener('click', () => {
+      getRightAnswer.style.backgroundColor = 'green';
+    }, false);
+  };
   http.send();
 }
